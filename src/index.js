d06b2d56db876ba0b3b53405b12c0042cb5a28bd
flaviotech <flavio.nogueira.profissional@gmail.com>
2022-04-15 08:48:33 -0300

    Aula 19

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import ListComponent from "./ListComponent";
 
-function HelloWorld() {
-  return (
-    <h1>Hello, World!</h1>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    <HelloWorld />
+    <ListComponent />
   </React.StrictMode>
 );
 

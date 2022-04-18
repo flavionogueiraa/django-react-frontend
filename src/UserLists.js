082bfb7a58c22547ec135e79472f25d07083f243
flaviotech <flavio.nogueira.profissional@gmail.com>
2022-04-18 06:38:42 -0300

    Aula 24

--- src/UserLists.js
+++ src/UserLists.js
@@ -8,8 +8,15 @@ export default class UserLists extends React.Component {
   };
 
   async componentDidMount() {
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+        Authorization: "Token 955b30a7bd5ed85a7c067e33c5ed12e99d24e1f5",
+      },
+    };
+
     const url = "http://localhost:8000/core/list/";
-    const response = await fetch(url);
+    const response = await fetch(url, config);
     const data = await response.json();
 
     console.log(data);

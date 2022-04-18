dd9160aa9b99b9391677569b06c19a493ec71fce
flaviotech <flavio.nogueira.profissional@gmail.com>
2022-04-18 06:34:13 -0300

    Aula 22

--- src/UserLists.js
+++ src/UserLists.js
@@ -1,11 +1,30 @@
 import React from "react";
 import ListComponent from "./ListComponent";
 
-export default function UserLists() {
-  return (
-    <div>
-      <ListComponent listName="Minha lista"/>
-      <ListComponent listName="Minha lista 2"/>
-    </div>
-  );
+export default class UserLists extends React.Component {
+  state = {
+    lists: null,
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const url = "http://localhost:8000/core/list/";
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data);
+    this.setState({
+      list: data,
+      loading: false,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ListComponent listName="Minha lista" />
+        <ListComponent listName="Minha lista 2" />
+      </div>
+    );
+  }
 }
